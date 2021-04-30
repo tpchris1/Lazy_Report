d80dc5b7327d985553acabac5733d1bd533653cd
105062325 <topchrischang@hotmail.com>
2021-04-30 21:19:46 +0800

    增加了heroku部署相關資訊

--- mylinebot/settings.py
+++ mylinebot/settings.py
@@ -16,6 +16,8 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# 給Heroku用到的static文件夾 沒有具體意義
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
